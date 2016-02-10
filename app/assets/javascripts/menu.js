074b89dfac72edd5d29e74ac018c666b9b48096a
Julian Bouma <bouma@post.harvard.edu>
2016-02-10 13:50:11 -0800

    with remote true

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -3,24 +3,9 @@ $(document).ready(function() {
     $('#new_menu').toggleClass('hidden');
   });
 
-  $('#new_menu').on('submit', function(event) {
-    event.preventDefault();
-    var form = this;
-    $.ajax({
-      url: form.action,
-      method: form.method,
-      data: $(form).serialize(),
-      dataType: 'html'
-    }).done(function(response) {
-      // var element = "<li class='list-group-item'><a href='/menu/" + response.id + "'>" + response.name + "</a></li>";
-      // $('ul.menus.list-group').append(element);
-      // $('#new_menu input[type=text]').val('');
-      // $('#new_menu').toggleClass('hidden');
-      $('ul.menus.list-group').append(response);
-      $('#new_menu input[type=text]').val('');
-      $('#new_menu').toggleClass('hidden');
-    }).fail(function(response) {
-      console.log("Got a bad response: " + response)
-    });
+  $('#new_menu').on('ajax:success', function(event, data, status, response) {
+    $('ul.menus.list-group').append(data);
+    $('#new_menu input[type=text]').val('');
+    $('#new_menu').toggleClass('hidden');
   });
 })
